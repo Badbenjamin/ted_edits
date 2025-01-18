a83f659235b2a1852a32840a8b1d531f258f347d
ben mccarthy <bmccarthy90@gmail.com>
2025-01-18 12:54:23 -0500

    set up react router

--- ted_edits/src/main.jsx
+++ ted_edits/src/main.jsx
@@ -1,10 +1,25 @@
+
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, ReactDOM } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import ErrorElement from './ErrorElement.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorElement />,
+    children: []
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
