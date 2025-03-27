7a5ca5a68bdb93eaa598e5a363927d65b65740cd
ben mccarthy <bmccarthy90@gmail.com>
2025-03-27 18:06:14 -0400

    about page image resizing

--- ted_edits/src/About.jsx
+++ ted_edits/src/About.jsx
@@ -1,13 +1,16 @@
 import TopBar from "./TopBar"
+import Footer from "./Footer"
 import TedPic from "./assets/ted pic.jpg"
 
 export default function About(){
     return(
         <div className="app-container">
-            <TopBar/>
-            <div className="edit-content">
+            <div className="header">
+                <TopBar/>
+            </div>
+            <div className="about-container">
                 <div className="about-image">
-                    <img src={TedPic}/>
+                    <img className="ted-pic" src={TedPic}/>
                 </div>
                 <div className="about-paragraph">
                     <p>Ted Eschweiler is a Brooklyn based editor with experience in documentary, branded content, and a specialty in adult animated comedy. His work has been featured every which way TV spews including on Showtime, Comedy Central, and Paramount Plus. In his free time away from an edit bay, Ted likes to bike all through New York, hike outside of it, and generally be in the woods.
@@ -16,6 +19,10 @@ export default function About(){
                     <p>Ted.Eschweiler@gmail.com</p>
                 </div>
             </div>
+            <div className="footer">
+                <Footer/>
+            </div>
+            
         </div>
         
         
