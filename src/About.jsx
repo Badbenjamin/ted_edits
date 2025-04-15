1fd907b6334cca5937efc53db79197128ce8a570
ben mccarthy <bmccarthy90@gmail.com>
2025-04-14 22:05:59 -0400

    updated about copy

--- src/About.jsx
+++ src/About.jsx
@@ -16,6 +16,8 @@ export default function About(){
                     <p>Ted Eschweiler is a Brooklyn based editor with experience in documentary, branded content, and a specialty in adult animated comedy. His work has been featured across broadcast and streaming platforms including Showtime, Comedy Central, and Paramount Plus.
                     </p>
                     <br></br>
+                    <p>In his free time away from an edit bay, Ted likes to bike through New York, hike outside of it, and generally be in the woods.</p>
+                    <br></br>
                     <p>Let’s make a thing.</p>
                     <br></br>
                     <p>Ted.Eschweiler@gmail.com</p>
