4b44c4a724891d29103f72ff67a664287bb67387
ben mccarthy <bmccarthy90@gmail.com>
2025-04-14 21:55:07 -0400

    thumbnail centered in css

--- src/Thumbnail.jsx
+++ src/Thumbnail.jsx
@@ -2,23 +2,23 @@
 
 export default function ThumbNail({gif, windowSize}){
     let width = 600
-    
+    let height = width*.5642
 
     if (windowSize.width >= 900){
         console.log('over 900')
         width = 400
-        // height = 250
+        height = width*.5642
 
     } else if (windowSize.width >= 600 && windowSize.width <= 900){
         console.log('btw 600 and 900')
         width = 300
-        // height = 200
+        height = width*.5642
     } else if (windowSize.width < 600) {
         console.log('> 600')
         width = 300
-        // height = 200
+        height = width*.5642
     }
-    let height = width*.5642
+    // let height = width*.5642
     return(
         <div>
             <img width={width} height={height}  className="thumbnail" src={gif} alt='alt text'/>
